2af0774f95ca35c23ad5273514d9f7bee7e46b1c
DeuryZ <119361476+DeuryZ@users.noreply.github.com>
2024-03-12 22:30:49 -0500

    feat: :tada: Garden shop project started
    The project structure with branches was created

---
+++ src/main/java/com/example/TBTDD/TbtddApplication.java
@@ -0,0 +1,13 @@
+package com.example.TBTDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbtddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TBTDD/TbtddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TBTDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbtddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
